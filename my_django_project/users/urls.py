933e1386e66864ebf9e131cd4eae1787d59f86b9
niyaa <niyanoufal369@gmail.com>
2025-02-09 00:59:14 +0530

    homepathchanged

--- my_django_project/users/urls.py
+++ my_django_project/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import register, user_login, home
 
 urlpatterns = [
+    path('home/', home, name='home'),
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
-    path('/', home, name='home'),
 ]
